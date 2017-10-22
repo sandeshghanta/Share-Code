3cd9e5c224eeec1ee0f9ee3673404796ce089d2c
kurnifamili <kurnifamili@gmail.com>
2017-10-22 14:20:03 +0800

    Fix indentations, typo, and a break statement

--- send.py
+++ send.py
@@ -1,19 +1,31 @@
 import os
 import sys
 import requests
-filename = sys.argv[1]
-flag = False
-for i in (os.listdir(os.getcwd())):
-    if (i == filename):
-        flag = True
-if (not flag):
-    print filename + " file not found"
-    exit(0)
-falg = False
-files = {'file': open(filename, 'rb')}
-password = str(raw_input("Enter password for your file "))
-if (password == ""):
-    password = "null"
-r = requests.post("http://sharecode.co.nf/server.php?filename="+filename+"&password="+password,files=files)
-print r.text
 
+
+def main() {
+   filename = sys.argv[1]
+   flag = False
+
+   for i in (os.listdir(os.getcwd())):
+       if (i == filename):
+           flag = True
+           break
+
+   if (not flag):
+       print filename + " file not found"
+       exit(0)
+
+   flag = False
+   files = {'file': open(filename, 'rb')}
+   password = str(raw_input("Enter password for your file "))
+
+   if (password == ""):
+       password = "null"
+   r = requests.post("http://sharecode.co.nf/server.php?filename="+filename+"&password="+password,files=files)
+   print r.text
+}
+
+
+if __name__ == "__main__":
+   main()
